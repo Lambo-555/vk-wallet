676c95d9db48d798269675c874ff3acb1801e1be
Stanislav Malyshev <smalyshev@sfxdx.com>
2022-04-15 22:00:06 +0300

    commit useless pages - home, persic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+// import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
-import bridge from '@vkontakte/vk-bridge';
+// import bridge from '@vkontakte/vk-bridge';
 import {
 	View,
 	ScreenSpinner,
@@ -10,8 +11,8 @@ import {
 } from '@vkontakte/vkui';
 import '@vkontakte/vkui/dist/vkui.css';
 
-import Home from './panels/Home';
-import Persik from './panels/Persik';
+// import Home from './panels/Home';
+// import Persik from './panels/Persik';
 
 import Menu from './panels/Menu';
 import Wallet from './panels/Wallet';
@@ -23,30 +24,30 @@ import Vault from './panels/Vault';
 const App = () => {
 	const activePanel = useSelector((state) => state.panel.panel);
 	const isLoading = useSelector((state) => state.wallet.isLoading);
-	const [fetchedUser, setUser] = useState(null);
+	// const [fetchedUser, setUser] = useState(null);
 
-	useEffect(() => {
-		bridge.subscribe(({ detail: { type, data } }) => {
-			if (type === 'VKWebAppUpdateConfig') {
-				const schemeAttribute = document.createAttribute('scheme');
-				schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
-				document.body.attributes.setNamedItem(schemeAttribute);
-			}
-		});
-		async function fetchData() {
-			const user = await bridge.send('VKWebAppGetUserInfo');
-			setUser(user);
-			// setPopout(null);
-		}
-		fetchData();
-	}, []);
+	// useEffect(() => {
+	// 	bridge.subscribe(({ detail: { type, data } }) => {
+	// 		if (type === 'VKWebAppUpdateConfig') {
+	// 			const schemeAttribute = document.createAttribute('scheme');
+	// 			schemeAttribute.value = data.scheme ? data.scheme : 'client_light';
+	// 			document.body.attributes.setNamedItem(schemeAttribute);
+	// 		}
+	// 	});
+	// 	async function fetchData() {
+	// 		const user = await bridge.send('VKWebAppGetUserInfo');
+	// 		setUser(user);
+	// 		// setPopout(null);
+	// 	}
+	// 	fetchData();
+	// }, []);
 
 	return (
 		<AdaptivityProvider>
 			<AppRoot>
 				<View activePanel={activePanel} popout={isLoading && <ScreenSpinner size="large" />}>
-					<Home id="home" fetchedUser={fetchedUser} />
-					<Persik id="persik" />
+					{/* <Home id="home" fetchedUser={fetchedUser} /> */}
+					{/* <Persik id="persik" /> */}
 					<Menu id="menu" />
 					<Wallet id="wallet" />
 					<Transaction id="transaction" />
